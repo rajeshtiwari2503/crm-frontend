ddf2d4aea7b273ab4cc28f5153e0adb898604042
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2024-07-10 17:18:29 +0530

    order

--- src/app/inventory/order/page.jsx
+++ src/app/inventory/order/page.jsx
@@ -13,24 +13,77 @@ const Order = () => {
 
   const [order, setOrder] = useState([])
   const [refresh, setRefresh] = useState("")
+  const [sparepart, setSparepart] = useState([])
+  const [serviceCenter, setServiceCenter] = useState([])
+  const [brands, setBrands] = useState([])
+  const [value, setValue] = React.useState(null);
 
   useEffect(() => {
+    const storedValue = localStorage.getItem("user");
+    if (storedValue) {
+      setValue(JSON.parse(storedValue));
+    }
     getAllOrder()
-
+    getAllSparepart()
+    getAllServiceCenter()
+    getAllBrand()
   }, [refresh])
 
   const getAllOrder = async () => {
+    const storedValue = localStorage.getItem('user');
+    const userInfo = storedValue ? JSON.parse(storedValue) : null;
+  
     try {
-      let response = await http_request.get("/getAllOrder")
+      let endPoint=
+     userInfo?.user?.role === 'ADMIN'? 
+         '/getAllOrder'
+        : userInfo?.user?.role === 'BRAND'? 
+         `/getAllOrderById?brandId=${userInfo?.user?._id}`
+         : `/getAllOrderById?serviceCenterId=${userInfo?.user?._id}`
+     
+  
+      let response = await http_request.get(endPoint);
+      let { data } = response;
+  
+      setOrder(data);
+    } catch (err) {
+      console.error('Error fetching orders:', err);
+    }
+  };
+
+  const getAllSparepart = async () => {
+    try {
+      let response = await http_request.get("/getAllSparePart")
       let { data } = response;
 
-      setOrder(data)
+      setSparepart(data)
     }
     catch (err) {
       console.log(err);
     }
   }
+  const getAllServiceCenter = async () => {
+    try {
+      let response = await http_request.get("/getAllService")
+      let { data } = response;
 
+      setServiceCenter(data)
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+  const getAllBrand = async () => {
+    try {
+      let response = await http_request.get("/getAllBrand")
+      let { data } = response;
+
+      setBrands(data)
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
   const data = order?.map((item, index) => ({ ...item, i: index + 1 }));
 
   const RefreshData = (data) => {
@@ -41,7 +94,7 @@ const Order = () => {
     <Sidenav>
       <Toaster />
       <>
-        <OrderList data={data} RefreshData={RefreshData} />
+        <OrderList data={data} userData={value} brand={brands} serviceCenter={serviceCenter} sparepart={sparepart} RefreshData={RefreshData} />
       </>
     </Sidenav>
   )

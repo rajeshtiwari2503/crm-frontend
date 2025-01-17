d65adb7b728d77e4c9acca093c64683ee94679a0
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2025-01-17 16:27:32 +0530

    fhjhjhjhjhjf

--- src/app/sign_in/page.jsx
+++ src/app/sign_in/page.jsx
@@ -176,16 +176,16 @@ export default function SignIn() {
                     Sign Up
                   </Link>
                 </p>
-                <div className="signin-page flex flex-col items-center justify-center p-2 rounded-md bg-gray-100">
+                {/* <div className="signin-page flex flex-col items-center justify-center p-2 rounded-md bg-gray-100">
                   <div className="text-md text-gray-700 mb-2">Please download our app directly ---</div>
 
-                  {/* Download Button */}
+                
                   <a href={apkDownloadUrl} download>
                     <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-2 rounded">
                       Download APK
                     </button>
                   </a>
-                </div>
+                </div> */}
 
               </div>
             </div>

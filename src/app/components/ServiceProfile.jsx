4555ed52498e3881d00da6f27359faa41db5a5e8
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2025-05-17 11:54:28 +0530

    fjkkjkjf

--- src/app/components/ServiceProfile.jsx
+++ src/app/components/ServiceProfile.jsx
@@ -6,6 +6,8 @@ import ServicePincodes from './ServicePincodes';
 import AddSupportedBrands from '../user/service/details/[id]/AddSupportedBrands';
 import AddCategory from '../user/service/details/[id]/AddCategory';
 import ServiceCenterDepositForm from './ServiceCenterDeposit';
+import { Toaster } from 'react-hot-toast';
+import { ToastMessage } from './common/Toastify';
 
 const ServiceProfile = (props) => {
 
@@ -13,6 +15,9 @@ const ServiceProfile = (props) => {
   const [uploadedGstImage, setUploadedGstImage] = useState(null);
   const [uploadedProofImage, setUploadedProofImage] = useState(null);
   const [uploadedDocImage, setUploadedDocImage] = useState(null);
+  const [uploadedQrCode, setUploadedQrCode] = useState(null);
+   const [UPIid, setUPIid] = useState("");
+
   const { userData } = props
   const uploadGst = async (img) => {
     try {
@@ -33,6 +38,53 @@ const ServiceProfile = (props) => {
       console.log(err);
     }
   }
+  const uploadQrCode = async (img) => {
+    try {
+      setLoading(true)
+      const formData = new FormData();
+      formData.append("qrCode", uploadedQrCode);
+      let response = await http_request.patch(`/uploadCenterQrCode/${userData?._id}`, formData);
+      let { data } = response;
+      props?.RefreshData(data)
+      window.location.reload();
+      ToastMessage(data);
+      setUploadedQrCode(null)
+      setLoading(false)
+
+    } catch (err) {
+      setLoading(false)
+
+      console.log(err);
+    }
+  }
+  const uploadUPIid = async ( ) => {
+  try {
+    setLoading(true);
+
+    const response = await http_request.patch(
+      `/uploadCenterUPIid/${userData?._id}`,
+      { UPIid: UPIid } // Send UPI ID as JSON
+    );
+
+    const { data } = response;
+    console.log("data",data);
+    
+    props?.RefreshData(data);
+    ToastMessage(data);
+    setLoading(false);
+  } catch (err) {
+    console.error(err);
+    setLoading(false);
+  }
+};
+
+  const handleFileChangeQrCode = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setUploadedQrCode(file);
+
+    }
+  };
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -95,6 +147,7 @@ const ServiceProfile = (props) => {
   return (
 
     <div className=' '>
+      <Toaster />
       {loading === true ? <ReactLoader />
         :
         <div className="md:m-5 m-0 grid md:grid-cols-4 sm:grid-cols-2 grid-cols-2 mt-5 gap-1">
@@ -156,6 +209,69 @@ const ServiceProfile = (props) => {
                 <label className="text-sm">Upload Certification Documents</label>
                 <input type="file" {...register('certificationDocuments')} />
             </div> */}
+          <div className="mb-4 md:col-span-3 flex gap-4 items-center mt-4">
+            <label className="block text-gray-700 font-medium mb-2">UPI_ID</label>
+            <div>{userData?.UPIid}  </div>
+            <input
+              type="text"
+              value={UPIid}
+              onChange={(e) => setUPIid(e.target.value)}
+              className="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-200"
+              placeholder="example@upi"
+              required
+            />
+          </div>
+          <div className="mb-4 mt-4 md:text-end">
+            <button
+            onClick={() => uploadUPIid( )}
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+            >
+              Upload UPI 
+            </button>
+          </div>
+          <div className=' md:text-1xl text-sm font-semibold mt-5'>Paymemt Qr Code :</div>
+          <div className=' md:text-1xl text-sm  mt-5 '>
+            <img
+              src={userData?.qrCode}
+              height="100px"
+              width="100px"
+              className='m-2'
+              alt=''
+            />
+          </div>
+          <div className='dz-message flex items-center justify-center'>
+
+            <div className=''>
+              {
+                uploadedQrCode ?
+                  <img
+                    src={URL.createObjectURL(uploadedQrCode)}
+                    height="100px"
+                    width="100px"
+                    className='m-2'
+                    alt=''
+                  /> :
+                  ""
+              }
+              <input
+                id='filesize'
+                onChange={handleFileChangeQrCode}
+                name="file"
+                type="file"
+                accept=".jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff, .mp4, .webm, .mp3, .wav, .ogg, .glb"
+                className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+              />
+            </div>
+          </div>
+          <div className="flex items-center  md:ms-8  justify-center">
+            <button
+              onClick={uploadQrCode}
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Upload Qr Code
+            </button>
+          </div>
           <div className=' md:text-1xl text-sm font-semibold'>Gst Certificate :</div>
           <div className=' md:text-1xl text-sm   '>
             <img

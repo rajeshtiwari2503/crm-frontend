475eba93458456e44c1b788d4006558649f28a7c
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2025-05-27 14:47:49 +0530

    fjhjhjhjf

--- src/app/warrantyActivations/page.jsx
+++ src/app/warrantyActivations/page.jsx
@@ -4,8 +4,9 @@ import React, { useEffect, useState } from 'react'
 import http_request from '.././../../http-request'
 import WarrantyActivationList from './activationList'
 import { useUser } from '../components/UserContext'
+import { ReactLoader } from '../components/common/Loading'
 
- 
+
 
 const WarrantyActivation = (props) => {
   const [warrantyActivation, setWarrantyActivation] = useState([])
@@ -13,42 +14,62 @@ const WarrantyActivation = (props) => {
 
   const [refresh, setRefresh] = useState("")
   const [userData, setUserData] = React.useState(null);
-   const { user } = useUser();
-             
-            
-             useEffect(() => {
-              
-               if (user) {
-                setUserData(user);
-               }
-     
-    
-    getAllWarrantyActivation()
-    getAllProduct()
-  }, [refresh,user])
+  const { user } = useUser();
+  const [loading, setLoading] = useState(false);
 
+  // Pagination state
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(10); // You can make this dynamic
+  const [totalPages, setTotalPages] = useState(1);
 
-  const getAllWarrantyActivation = async () => {
-    try{
-      let response = await http_request.get("/getAllActivationWarranty")
-      let { data } = response;
-      // console.log(data);
-  
-      setWarrantyActivation(data)
+  useEffect(() => {
+    if (user) {
+      setUserData(user);
     }
-   
-    catch(err){
+
+    getAllWarrantyActivation(page);
+    getAllProduct();
+  }, [refresh, user, page]);
+
+
+  const getAllWarrantyActivation = async (pageNum) => {
+    setLoading(true); // Start loading
+    try {
+      const response = await http_request.get(
+        `/getAllActivationWarrantyWithPage?page=${pageNum}&limit=${limit}`
+      );
+      const { data } = response;
+      setWarrantyActivation(data?.data || []);
+      setTotalPages(data?.totalPages || 1);
+    } catch (err) {
       console.log(err);
-      
+    } finally {
+      setLoading(false); // Stop loading
     }
-  }
+  };
+
+
+  // const getAllWarrantyActivation = async () => {
+  //   try{
+  //     let response = await http_request.get("/getAllActivationWarranty")
+  //     let { data } = response;
+  //     // console.log(data);
+
+  //     setWarrantyActivation(data)
+  //   }
+
+  //   catch(err){
+  //     console.log(err);
+
+  //   }
+  // }
   const getAllProduct = async () => {
     let response = await http_request.get("/getAllProduct")
     let { data } = response;
 
     setProduct(data)
   }
-  const filterData = userData?.user?.role === "ADMIN" ? warrantyActivation :userData?.user?.role === "BRAND EMPLOYEE"? warrantyActivation?.filter((f) => f?.brandId === userData?.user?.brandId):warrantyActivation?.filter((f) => f?.brandId === userData?.user?._id)
+  const filterData = userData?.user?.role === "ADMIN" ? warrantyActivation : userData?.user?.role === "BRAND EMPLOYEE" ? warrantyActivation?.filter((f) => f?.brandId === userData?.user?.brandId) : warrantyActivation?.filter((f) => f?.brandId === userData?.user?._id)
 
   const data = filterData?.map((item, index) => ({ ...item, i: index + 1 }));
 
@@ -58,11 +79,25 @@ const WarrantyActivation = (props) => {
 
   return (
     <>
-       
-        <Sidenav>
-          <WarrantyActivationList data={data} userData={userData?.user} brandData={props?.brandData} product={product} RefreshData={RefreshData} />
-        </Sidenav>
-    
+
+      <Sidenav>
+        {loading === true ? (
+          <div className="flex items-center justify-center h-[80vh]">
+            <ReactLoader />
+          </div>
+        ) : (
+          <WarrantyActivationList
+            page={page}
+            setPage={setPage}
+            limit={limit}
+            setLimit={setLimit}
+
+            totalPage={totalPages}
+            data={data}
+            userData={userData?.user} brandData={props?.brandData} product={product} RefreshData={RefreshData} />
+        )}
+      </Sidenav>
+
     </>
   )
 }

5bc025695b34ac5f7b9484551fba6d54982c1535
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2025-06-25 17:01:04 +0530

    fjhjhjfhjfdghfghf

--- src/app/courier-services/orders/OrderList.jsx
+++ src/app/courier-services/orders/OrderList.jsx
@@ -11,7 +11,7 @@ import { Toaster } from 'react-hot-toast';
 import http_request from '.././../../../http-request'
 import { ReactLoader } from '@/app/components/common/Loading';
 import { useForm } from 'react-hook-form';
-import SparePartsForm from './addOrder';
+ 
 import CreateOrderDialog from './addOrder';
 
 
